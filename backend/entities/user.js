9c47ce595542764d31a0522911720ff3de940783
beccac14-bit <beccachen860214@gmail.com>
2026-08-13 16:29:44 +0800

    Add User entity schema for TypeORM

---
+++ backend/entities/user.js
@@ -0,0 +1,31 @@
+const { EntitySchema } = require('typeorm')
+
+module.exports = new EntitySchema({
+  name: 'User',
+  tableName: 'USER',
+  columns: {
+    id: { primary: true,
+         type: 'uuid',
+          generated: 'uuid',
+           nullable: false },
+    name: { type: 'varchar',
+         length: 100,
+          nullable: false },
+    email: { type: 'varchar',
+            length: 100,
+         nullable: false },
+    password: { type: 'varchar',
+               length: 225,
+         nullable: false },
+    role: { type: 'varchar',
+           length: 50,
+           nullable: false },
+    created_at: { type: 'timestamp',
+         createDate: true,
+          nullable: false },
+    updated_at: { type: 'timestamp',
+         updateDate: true,
+          nullable: false }
+     }
+  },
+})
