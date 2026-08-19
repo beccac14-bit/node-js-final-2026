0556d8a18b4e226d1bafaabff5c2fdf150c38433
beccac14-bit <beccachen860214@gmail.com>
2026-08-19 18:06:55 +0800

    Rename entity and table for credit package purchases

---
+++ backend/entities/credit_package_purchase.js
@@ -0,0 +1,39 @@
+const { EntitySchema } = require('typeorm');
+
+module.exports = new EntitySchema({
+  name: 'CreditPackagePurchase',
+  tableName: 'CREDIT_PACKAGE_PURCHASE',
+  columns: {
+    id: {
+      primary: true,
+      type: 'uuid',
+      generated: 'uuid',
+      nullable: false,
+    },
+    user_id: {
+      type: 'uuid',
+      nullable: false,
+    },
+    package_id: {
+      type: 'uuid',
+      nullable: false,
+    },
+    buy_at: {
+      type: 'timestamp',
+      createDate: true,
+      nullable: false,
+    },
+  },
+  relations: {
+    user: {
+      target: 'User',
+      type: 'many-to-one',
+      joinColumn: { name: 'user_id' },
+    },
+    package: {
+      target: 'CreditPackage',
+      type: 'many-to-one',
+      joinColumn: { name: 'package_id' },
+    },
+  },
+});
