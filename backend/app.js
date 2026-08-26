e9c74dd521c02462950dce16c532357c7bc7edc7
beccac14-bit <beccachen860214@gmail.com>
2026-08-26 14:29:14 +0800

    Update API routes documentation in app.js

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 // GET 取得教練技能列表、POST 新增教練技能、DELETE 刪除教練技能
 app.use('/api/coaches/skill', skillRouter);
 
-// GET 取得購買方案列表、POST 新增購買方案、DELETE 刪除購買方案
+// GET 取得購買方案列表、POST 新增購買方案、DELETE 刪除購買方案、POST 購買堂數方案（需登入）
 app.use('/api/credit-package', packageRouter);
 
 // POST 註冊新會員帳號、POST 會員登入、GET 取得個人資料、PUT 更新本人的暱稱、PUT 修改本人的登入密碼
