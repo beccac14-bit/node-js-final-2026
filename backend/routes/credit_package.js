d690978c6363333ba96d357ee5672da9611fd34f
beccac14-bit <beccachen860214@gmail.com>
2026-08-26 14:28:31 +0800

    Add userBuyPackage route with token verification

--- backend/routes/credit_package.js
+++ backend/routes/credit_package.js
@@ -4,13 +4,13 @@ const router = express.Router();
 const creditPackageController = require('../controllers/creditPackageController');
 const checkPackageBody = require('../middlewares/checkPackageBody');
 const checkPackageId = require('../middlewares/checkPackageId');
+const verifyToken = require('../middlewares/verifyToken');
 
 
 router.get('/', creditPackageController.getPackages);
 router.post('/', checkPackageBody, creditPackageController.postPackages);
 router.delete('/:creditPackageId', checkPackageId, creditPackageController.deletePackages);
-
+router.post('/:creditPackageId', verifyToken, checkPackageId, creditPackageController.userBuyPackage);
  
-
 
 module.exports = router;
